8089639db51a2404ba9e17e1f780d95a750dad74
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-02-13 14:51:59 -0800

    adds favicon

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
+
 const app = express();
 
 //fix this
@@ -8,5 +10,6 @@ app.get('/', function(req, res) {
 });
 
 app.use(express.static('static'))
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.listen(3000, () => console.log('app running on port 3000'))
