b8e68e1ae535ab50e46982f48b7f12a29489cfa0
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-02-26 20:09:15 -0800

    adds views

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3000;
 var app = express();
   app.use(express.static(path.join(__dirname, 'public')))
   app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
+  app.set('view engine', 'ejs');
+  app.get('/', (req, res) => res.render('pages/index.ejs'));
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
   app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
